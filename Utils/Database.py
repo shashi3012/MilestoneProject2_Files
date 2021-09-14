05629b9f4da27a0f3e02b2c4743120b5821b8e7f
Shashi Shekhar Labh <sshekharlabh@gmail.com>
2021-09-14 22:39:21 +0530

    Adding files to the repository

---
+++ Utils/Database.py
@@ -0,0 +1,26 @@
+"""
+
+Concerned with storing and retrieving books from a list.
+
+
+"""
+books = []
+
+
+def add_books(name, author):
+    books.append({'name': name, 'author': author, 'read': False})
+
+
+def get_all_books():
+    return books
+
+
+def mark_book_as_read(name):
+    for book in books:
+        if book['name'] == name:
+            book['read'] = True
+
+
+def delete_book(name):
+    global books
+    books = [book for book in books if book['name'] != name]
